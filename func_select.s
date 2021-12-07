d1931935be0390e8c534baa3e4871e5a607661b0
dan <funkey7dan@gmail.com>
2021-12-07 22:02:00 +0200

    Need to fix the replace function

--- func_select.s
+++ func_select.s
@@ -18,9 +18,9 @@
 d:
        .string "%d"
 s:
-       .string "%s"
+       .string " %s"
 c:
-       .string "%c"
+       .string " %c"
 #printf formats
 print:
         .string "out: = %s\n"
@@ -43,12 +43,8 @@ run_func:
         #function start
 
         #save registers
-        pushq %r10
-        pushq %r11
-        pushq %r12
-        pushq %r13
-        pushq %rbp
-        movq %rsp, %rbp
+
+
         # swith case
         subq $50,%rdi #check index
         cmpq $10,%rdi #check if out of bounds
@@ -56,6 +52,8 @@ run_func:
         jmp *.VALUES(,%rdi,8)
 #case 50 or 60 length
 .L3:
+    pushq %r10
+    pushq %r11
     movq %rsi,%rdi # move the pointer to pstring1 from the 2nd arg to the 1st
     call pstrlen
     movq %rax,%r10 # save the returned size of pstring1
@@ -63,54 +61,76 @@ run_func:
     call pstrlen
     movq %rax,%r11 # save the returned size of pstring2
     #print
-    subq $16,%rsp #allign the stack before calling
-    movq $0,%rax # we shall clear the value of the return register
+    ##subq $16,%rsp #allign the stack before calling
+    xor %rax,%rax # we shall clear the value of the return register
     movq $len_print,%rdi # put the format into 1st argument
     leaq (%r10),%rsi #put the len of pstring1 to the 2nd arg
     leaq (%r11),%rdx #put the len of pstring2 to the 3rd arg
     call printf
+    popq %r11
+    popq %r10
     jmp .END
 #Case 52 replace chars
 .L4:
     pushq %rbp
     movq %rsp, %rbp
-    addq $-16,%rsp #get stack space
+    subq $32,%rsp #get stack space
+    #save registers
+    pushq %r10
+    pushq %r11
+    pushq %r12
+    pushq %r13
+    pushq %r14
+    pushq %r15
 
     #save passed args
     movq %rdx,%r13 #save pstr2
-    movq %rsi,%r14 #save pstr1
+    movq %rsi,%r12 #save pstr1
 
     #first scanf
-    movq $s,%rdi # put the format into 1st argument
+    mov $s,%rdi # put the format into 1st argument
     leaq -8(%rbp),%rsi #allocate bytes for scanf
-    subq $16,%rsp #allign the stack before calling
-    movq $0,%rax # we shall clear the value of the return register
+    #subq $8,%rsp #allign the stack before calling
+    xor %rax,%rax # we shall clear the value of the return register
     call scanf #get the old char
-    movzbq -8(%rbp),%r10 #move the old char to temp register to move to 2nd arg afterwards padded with zeros to prevent clutter
+    movzbq -8(%rbp),%r14 #save the old char to temp register to move to 2nd arg afterwards padded with zeros to prevent clutter
 
     #second scanf
-    #movq $c,%rdi # put the format into 1st argument
-    leaq -16(%rbp),%rsi #allocate bytes for scanf
-    subq $16,%rsp #allign the stack before calling
-    movq $0,%rax # we shall clear the value of the return register
-    call scanf #get the new char
-    movzbq -16(%rbp),%rdx #move the new char to 3rd arg padded with zeros to prevent clutter
+    movq $s,%rdi # put the format into 1st argument
+    leaq -17(%rbp),%rsi #allocate bytes for scanf
+    #subq $16,%rsp #allign the stack before calling
+    xor %rax,%rax # we shall clear the value of the return register
+    call scanf 
+    movzbq -17(%rbp),%r15 #move the new char to 3rd arg padded with zeros to prevent clutter
+
     # replace chars in both strings
-    movq %r10,%rsi # put the old char to 2nd arg
-    movq %r13,%rdi #move the first pstr to the first arg to call replaceChar
+    movq %r14,%rsi # put the old char to 2nd arg
+    movq %r15,%rdx # put the new char to 3rd arg
+    movq %r12,%rdi #move the first pstr to the 1st to call replaceChar
     call replaceChar
-    pushq %rax # save the pointer returned from replace on stack
-    movq %r14,%rdi #pop the second pstr to the first arg to call replaceChar
+    movq %rax,%r10 # save the pointer returned from replace
+    movq %r13,%rdi #move the second pstr to the first arg to call replaceChar
     call replaceChar
-    pushq %rax # save the pointer returned from replace on stack
+    movq %rax,%r11 # save the pointer returned from replace on stack
+
     #print the result
     movq $rplc_print,%rdi # put the format into 1st argument
     subq  $16,%rsp #allign the stack before calling
-    popq %r8 # put pstr2 to 5th argument
-    popq %rcx # put pstr1 to 4th argument
+    movq %r11,%r8 # put changed pstr2 to 5th argument
+    addq $8,%r8 # move the pointer to the string start
+    movq %r10,%rcx # put changed pstr1 to 4th argument
+    addq $8,%rcx # move the pointer to the string start
     # 1st arg format 2nd old char 3rd new char 4th pstr1 5th pstr2
     xor  %rax,%rax # we shall clear the value of the return register
     call  printf #print the string
+
+    # restore saved registers
+    popq %r15
+    popq %r14
+    popq %r13
+    popq %r12
+    popq %r11
+    popq %r10
     popq %rbp
     jmp .END
 #Case 53 copy
@@ -121,15 +141,15 @@ run_func:
         #first scanf
         movq $c,%rdi # put the format into 1st argument
         lea -1(%rbp),%rsi #allocate bytes for scanf
-        subq $16,%rsp #allign the stack before calling
-        movq $0,%rax # we shall clear the value of the return register
+        #subq $16,%rsp #allign the stack before calling
+        xor %rax,%rax # we shall clear the value of the return register
         call scanf #get the i index
         movb -1(%rbp),%dl #move the index i to 3rd arg
         #second scanf
         movq $c,%rdi # put the format into 1st argument
         lea -1(%rbp),%rsi #allocate bytes for scanf
-        subq $16,%rsp #allign the stack before calling
-        movq $0,%rax # we shall clear the value of the return register
+        #subq $16,%rsp #allign the stack before calling
+        xor %rax,%rax # we shall clear the value of the return register
         call scanf #get the j index from usr
         movb -1(%rbp),%cl #move the j index to 4th arg
         #function
@@ -143,7 +163,7 @@ run_func:
         movq %rax,%rsi # save the returned size of pstring1 in 2nd arg
         #printf the result
         movq $cpy_print,%rdi # move the format for printf to 1st arg
-        subq  $16,%rsp #allign the stack before calling
+        #subq  $16,%rsp #allign the stack before calling
         movq  $0,%rax # we shall clear the value of the return register
         call   printf #get the string
         jmp .END
@@ -192,15 +212,15 @@ run_func:
     #first scanf
     movq $c,%rdi # put the format into 1st argument
     lea -1(%rbp),%rsi #allocate bytes for scanf
-    subq $16,%rsp #allign the stack before calling
-    movq $0,%rax # we shall clear the value of the return register
+    #subq $16,%rsp #allign the stack before calling
+    xor %rax,%rax # we shall clear the value of the return register
     call scanf #get the old char
     movb -1(%rbp),%dl #move the index i to 3rd arg
     #second scanf
     movq $c,%rdi # put the format into 1st argument
     lea -1(%rbp),%rsi #allocate bytes for scanf
-    subq $16,%rsp #allign the stack before calling
-    movq $0,%rax # we shall clear the value of the return register
+    #subq $16,%rsp #allign the stack before calling
+    xor %rax,%rax # we shall clear the value of the return register
     call scanf #get the new char
     movb -1(%rbp),%cl #move the j index to 4th arg
     #function
@@ -224,10 +244,6 @@ run_func:
     jmp .END
 .END:
         #function end
-        movq %rbp,%rsp
-        popq %r11
-        popq %r10
-        popq %rbp
         ret
         
     
--- run_main.s
+++ run_main.s
@@ -19,48 +19,49 @@ run_main:
         # we will save pointer to pstring1 in r10 and to pstring2 in r11
         pushq %rbp
         movq %rsp, %rbp
-        subq $528,%rsp # allocate stack for 2 structs and int
+        subq $544,%rsp # allocate stack for 2 structs and int
 
         #initialize first pstring
         #first scanf for ps1
         movq $d,%rdi # put the format into 1st argument
-        leaq -8(%rbp),%rsi #allocate bytes for scanf
-        subq $16,%rsp #allign the stack before calling
-        movq $0,%rax # we shall clear the value of the return register
+        leaq -544(%rbp),%rsi #allocate bytes for scanf
+        ##subq $16,%rsp #allign the stack before calling
+        xor %rax,%rax # we shall clear the value of the return register
         call scanf #get length
 
         #second scanf for ps1
         movq $s,%rdi # put the string format into 1st argument
-        leaq -263(%rbp),%rsi #allocate 255+1 bytes for scanf
-        subq  $16,%rsp #allign the stack before calling
-        movq $0,%rax # we shall clear the value of the return register
+        leaq -536(%rbp),%rsi #allocate 255+1 bytes for scanf
+        #subq  $16,%rsp #allign the stack before calling
+        xor %rax,%rax # we shall clear the value of the return register
         call   scanf #get the string
 
         #initialize second pstring
         #first scanf for ps2
         movq $d,%rdi # put the format into 1st argument
-        leaq -271(%rbp),%rsi #allocate bytes for scanf
-        subq $16,%rsp #allign the stack before calling
-        movq $0,%rax # we shall clear the value of the return register
+        leaq -280(%rbp),%rsi #allocate bytes for scanf
+        ##subq $16,%rsp #allign the stack before calling
+        xor %rax,%rax # we shall clear the value of the return register
         call scanf #get length
 
         #second scanf for ps2
         movq $s,%rdi # put the string format into 1st argument
-        leaq -526(%rbp),%rsi #allocate bytes for scanf
-        subq $16,%rsp #allign the stack before calling
-        movq $0,%rax # we shall clear the value of the return register
+        leaq -272(%rbp),%rsi #allocate bytes for scanf
+        ##subq $16,%rsp #allign the stack before calling
+        xor %rax,%rax # we shall clear the value of the return register
         call   scanf #get the string
 
         #scanf for opt
         movq $d,%rdi # put the string format into 1st argument
-        leaq -534(%rbp),%rsi #allocate bytes for scanf
-        subq  $16,%rsp #allign the stack before calling
-        movq $0,%rax # we shall clear the value of the return register
+        leaq -16(%rbp),%rsi #allocate bytes for scanf
+        #subq  $16,%rsp #allign the stack before calling
+        xor %rax,%rax # we shall clear the value of the return register
         call   scanf #get the option
 
-        movq -534(%rbp),%rdi # put option into first argument
-        leaq -8(%rbp),%rsi # put the address of the first struct into second argument
-        leaq -271(%rbp),%rdx # put the address of the second struct into third argument
+        #function call
+        movq -16(%rbp),%rdi # put option into 1st argument
+        leaq -544(%rbp),%rsi # put the address of the first struct into 2nd argument
+        leaq -280(%rbp),%rdx # put the address of the second struct into 3rd argument
         call run_func
 
         # function finish
