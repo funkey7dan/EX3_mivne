ad2cb0eff132985dce0be2c32df3a8a9c0ee3a02
dan <funkey7dan@gmail.com>
2021-12-08 09:23:40 +0200

    Done  with copy+replace+len

--- func_select.s
+++ func_select.s
@@ -26,6 +26,8 @@ print:
         .string "out: = %s\n"
 default_format:
         .string "invalid option!\n"
+invalid_format:
+        .string "invalid input!\n"
 len_print:
         .string "first pstring length: %d, second pstring length: %d\n"
 rplc_print:
@@ -42,16 +44,14 @@ cmpr_print:
 run_func:
         #function start
 
-        #save registers
-
-
         # swith case
         subq $50,%rdi #check index
         cmpq $10,%rdi #check if out of bounds
-        ja .L8
+        ja .L8 # jump using table
         jmp *.VALUES(,%rdi,8)
 #case 50 or 60 length
 .L3:
+    # save registers
     pushq %r10
     pushq %r11
     movq %rsi,%rdi # move the pointer to pstring1 from the 2nd arg to the 1st
@@ -60,8 +60,8 @@ run_func:
     movq %rdx,%rdi # move the pointer to pstring2 from the 3rd arg to the 1st
     call pstrlen
     movq %rax,%r11 # save the returned size of pstring2
+
     #print
-    ##subq $16,%rsp #allign the stack before calling
     xor %rax,%rax # we shall clear the value of the return register
     movq $len_print,%rdi # put the format into 1st argument
     leaq (%r10),%rsi #put the len of pstring1 to the 2nd arg
@@ -70,11 +70,14 @@ run_func:
     popq %r11
     popq %r10
     jmp .END
+
 #Case 52 replace chars
 .L4:
+    #save stack
     pushq %rbp
     movq %rsp, %rbp
     subq $32,%rsp #get stack space
+
     #save registers
     pushq %r10
     pushq %r11
@@ -90,7 +93,6 @@ run_func:
     #first scanf
     mov $s,%rdi # put the format into 1st argument
     leaq -8(%rbp),%rsi #allocate bytes for scanf
-    #subq $8,%rsp #allign the stack before calling
     xor %rax,%rax # we shall clear the value of the return register
     call scanf #get the old char
     movzbq -8(%rbp),%r14 #save the old char to temp register to move to 2nd arg afterwards padded with zeros to prevent clutter
@@ -98,7 +100,6 @@ run_func:
     #second scanf
     movq $s,%rdi # put the format into 1st argument
     leaq -17(%rbp),%rsi #allocate bytes for scanf
-    #subq $16,%rsp #allign the stack before calling
     xor %rax,%rax # we shall clear the value of the return register
     call scanf 
     movzbq -17(%rbp),%r15 #move the new char to 3rd arg padded with zeros to prevent clutter
@@ -117,9 +118,7 @@ run_func:
     movq $rplc_print,%rdi # put the format into 1st argument
     subq  $16,%rsp #allign the stack before calling
     movq %r11,%r8 # put changed pstr2 to 5th argument
-    addq $8,%r8 # move the pointer to the string start
     movq %r10,%rcx # put changed pstr1 to 4th argument
-    addq $8,%rcx # move the pointer to the string start
     # 1st arg format 2nd old char 3rd new char 4th pstr1 5th pstr2
     xor  %rax,%rax # we shall clear the value of the return register
     call  printf #print the string
@@ -131,41 +130,92 @@ run_func:
     popq %r12
     popq %r11
     popq %r10
+    movq %rbp,%rsp #restore stack
     popq %rbp
     jmp .END
 #Case 53 copy
 .L5:
+        #save stack
+        pushq %rbp
+        movq %rsp, %rbp
+        subq $32,%rsp #get stack space
+
+        #save registers
+        pushq %r12
+        pushq %r13
+        pushq %r14
+        pushq %r15
+
         #save passed args
-        pushq %rdx #push pstr2 to stack
-        pushq %rsi #push pstr1 to stack
+        movq %rdx,%r13 #save pstr2
+        movq %rsi,%r12 #save pstr1
+
         #first scanf
-        movq $c,%rdi # put the format into 1st argument
-        lea -1(%rbp),%rsi #allocate bytes for scanf
-        #subq $16,%rsp #allign the stack before calling
+        mov $d,%rdi # put the format into 1st argument
+        leaq -8(%rbp),%rsi #allocate bytes for scanf
         xor %rax,%rax # we shall clear the value of the return register
-        call scanf #get the i index
-        movb -1(%rbp),%dl #move the index i to 3rd arg
+        call scanf #get i from user
+        movzbq -8(%rbp),%r14 # save i to register
+
         #second scanf
-        movq $c,%rdi # put the format into 1st argument
-        lea -1(%rbp),%rsi #allocate bytes for scanf
-        #subq $16,%rsp #allign the stack before calling
+        movq $d,%rdi # put the format into 1st argument
+        leaq -17(%rbp),%rsi #allocate bytes for scanf
         xor %rax,%rax # we shall clear the value of the return register
-        call scanf #get the j index from usr
-        movb -1(%rbp),%cl #move the j index to 4th arg
+        call scanf #get j from user
+        movzbq -17(%rbp),%r15 #save j to register
+
+        #check passed values
+        cmpq %r15,%r14 #check if i greater than j
+        jg .INVALID
+        # check if passed indices greater than length
+        cmpq (%r12),%r14
+        jg .INVALID
+        cmpq (%r12),%r15
+        jg .INVALID
+        cmpq (%r13),%r14
+        jg .INVALID
+        cmpq (%r13),%r15
+        jg .INVALID
+
         #function
-        popq %rsi #pop the second pstr to the second arg to call replaceChar
-        popq %rdi #pop the first pstr to the first arg to call function
+        movq %r12,%rdi # move first string to 1st arg, as dest
+        movq %r13,%rsi # move second string to 2nd arg, as src
+        movq %r14, %rdx #move i to 3rd arg
+        movq %r15, %rcx #move j to 4th arg
+
         call pstrijcpy
-        pushq %rax #save the returned pointer on stack
+        movq %rax,%r14 #save the returned pointer
+
         #get len of pstr1
         call pstrlen #on the pstr1 which is in %rdi
-        popq %rdx # move pstr1 to 3rd arg
+        movq %r14,%rdx # move pstr1 to 3rd arg
         movq %rax,%rsi # save the returned size of pstring1 in 2nd arg
+
+        #printf the changed string
+        movq $cpy_print,%rdi # move the format for printf to 1st arg
+        movq  $0,%rax # we shall clear the value of the return register
+        call  printf #print the string
+
+        #get len of pstr2
+        movq %r13,%rdi
+        addq $8,%r13
+        movq %r13,%rdx # move pstr2 to 3rd arg
+        call pstrlen #on the pstr2
+        movq %rax,%rsi # save the returned size of pstring1 in 2nd arg
+
         #printf the result
         movq $cpy_print,%rdi # move the format for printf to 1st arg
-        #subq  $16,%rsp #allign the stack before calling
         movq  $0,%rax # we shall clear the value of the return register
-        call   printf #get the string
+        call  printf #print the string
+
+        # restore saved registers
+        popq %r15
+        popq %r14
+        popq %r13
+        popq %r12
+
+        movq %rbp,%rsp #restore stack
+        popq %rbp
         jmp .END
 #Case 54 swapCase
 .L6:
@@ -179,6 +229,7 @@ run_func:
     movq %rax,%r10 #save the returned string1
     call swapCase
     movq %rax,%r11 #save the returned string2
+
     ##print result
     #print for str 1
     #get len of pstr1
@@ -242,6 +293,22 @@ run_func:
     movq  $0,%rax # we shall clear the value of the return register
     call   printf #get the string
     jmp .END
+
+.INVALID:
+    movq $invalid_format,%rdi # put the format into 1st argument
+        #subq  $16,%rsp #allign the stack before calling
+        movq  $0,%rax # we shall clear the value of the return register
+        call   printf #get the string
+        # restore saved registers
+        popq %r15
+        popq %r14
+        popq %r13
+        popq %r12
+
+        movq %rbp,%rsp #restore stack
+        popq %rbp
+        jmp .END
+
 .END:
         #function end
         ret
--- pstring.s
+++ pstring.s
@@ -12,26 +12,66 @@ pstrlen:
 # Pstring* replaceChar(Pstring* pstr, char oldChar, char newChar);
 replaceChar:
     movq $0,%rcx # rcx will be index j , set to zero
-    pushq %r12 # save register value
-    movq (%rdi),%r12 # save len to register
+    # save register values
+    pushq %r11
+    pushq %r12
+    # clear the value of registers
+    xor %r11,%r11
+    xor %r12,%r12
+
+    #save string part of pstr
+    movq %rdi,%r11
+    addq $8,%r11
+
+    movb (%rdi),%r12b # save len to register
+
     #for loop
     .LOOP_RPLC:
-        movb (%rdi,%rcx,1), %r8b # read pstr[j] into r8b
-        cmpb %r8b,%sil  # check if the current char is the old one
+        movzbq (%r11,%rcx,1), %r8 # read pstr[j] into r8
+        cmpq %r8,%rsi  # check if the current char is the old one
         jne .L41
-        movb %dl,(%rdi,%rcx,1) #replace the char
+        movb %dl,(%r11,%rcx,1) #replace the char
     .L41:
         addq $1,%rcx #increment the index
         cmpq %rcx,%r12
         jne .LOOP_RPLC
-    movq %rdi,%rax
+    movq %r11,%rax
     popq %r12
+    popq %r11
     ret
 #Pstring* pstrijcpy(Pstring* dst, Pstring* src, char i, char j);
 pstrijcpy:
+    #rdx holds i, rcx holds j
+
+    # save register values
+    pushq %r11
+    pushq %r12
+    # clear the value of registers
+    xor %r11,%r11
+    xor %r12,%r12
+    #save string part of pstr1
+    movq %rdi,%r11
+    addq $8,%r11
+    #save string part of pstr2
+    movq %rsi,%r12
+    addq $8,%r12
+    #for loop
+    .LOOP_CPY:
+        movzbq (%r12,%rdx,1), %r8  # read src[i] into temp register
+        movb  %r8b,(%r11,%rdx,1)
+        #cmpq %r8,%rsi  # check if the current char is the old one
+        #jne .L51
+        #movb %dl,(%r11,%rcx,1) #replace the char
+        addq $1,%rdx #increment the i index
+        cmpq %rcx,%rdx #check if index i greater than j
+        jle .LOOP_CPY
+    movq %r11,%rax
+    popq %r12
+    popq %r11
     ret
 #Pstring* swapCase(Pstring* pstr);
 swapCase:
+
     ret
 #int pstrijcmp(Pstring* pstr1, Pstring* psrt2, char i, char j);
 pstrijcmp:
